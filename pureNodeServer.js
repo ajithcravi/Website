bae2afcfb15c5d4184117384621529daa178b074
ajithcravi <craviajith@gmail.com>
2019-11-07 13:51:57 +0530

    read the url

--- pureNodeServer.js
+++ pureNodeServer.js
@@ -1,10 +1,12 @@
-const http = require('http');
+const http = require("http");
 
 handler = (req, res) => {
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.write('Hello, I am a web server');
-    res.end();
-}
+	console.log(req.url);
+
+	res.writeHead(200, { "Content-type": "text/plain" });
+	res.write("Hello, I am a web server");
+	res.end();
+};
 
 const server = http.createServer(handler);
 

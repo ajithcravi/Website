7fb995dc5b131443259d75c997fdd8964f4fbdb3
ajithcravi <craviajith@gmail.com>
2019-11-07 14:41:59 +0530

    pure node server completed

--- pureNodeServer.js
+++ pureNodeServer.js
@@ -1,11 +1,18 @@
 const http = require("http");
+const url = require("url");
 
 handler = (req, res) => {
-	console.log(req.url);
+	const parsedUrl = url.parse(req.url, true); //parse the url and 'true' commands to parse the query also
+	console.log(parsedUrl);
 
-	res.writeHead(200, { "Content-type": "text/plain" });
-	res.write("Hello, I am a web server");
-	res.end();
+	if (parsedUrl.pathname === "/") {
+		res.writeHead(200, { "Content-type": "text/plain" });
+		res.write("Hello, I am a web server");
+		res.end();
+	} else {
+		res.writeHead(404, { "Content-type": "text/plain" });
+		res.end();
+	}
 };
 
 const server = http.createServer(handler);

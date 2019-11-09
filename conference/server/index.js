adc658698dbca42f96a955f9d674e70c35d49b08
ajithcravi <craviajith@gmail.com>
2019-11-09 12:56:16 +0530

    added express.static middleware

--- conference/server/index.js
+++ conference/server/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 const routes = require("./routes");
-
+app.use(express.static("public"));
 app.use("/", routes());
 
+// app.get('/favico', (req,res,next))
 app.listen(3000, () => {
-  console.log("I am a express server listening in port 3000");
+  console.log("I am an express server listening in port 3000");
 });
 
 module.exports = app;

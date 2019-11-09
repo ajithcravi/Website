fc739f011aa96f3d5a6e980032884d395bd1ff9a
ajithcravi <craviajith@gmail.com>
2019-11-09 12:38:31 +0530

    feedback route addeed

---
+++ conference/server/routes/feedback/feedback.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = () => {
+  router.get("/", (req, res, next) => {
+    return res.send("Feedback Form");
+  });
+
+  router.post("/", (req, res, next) => {
+    return res.send("Form Sent");
+  });
+
+  return router;
+};
--- conference/server/routes/index.js
+++ conference/server/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const speakers = require("./speakers/speakers");
+const feedback = require("./feedback/feedback");
 
 module.exports = () => {
   router.get("/", (req, res, next) => {
@@ -9,6 +10,7 @@ module.exports = () => {
   });
 
   router.use("/speakers", speakers());
+  router.use("/feedback", feedback());
 
   return router;
 };

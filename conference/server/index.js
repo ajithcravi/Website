b431f0245f3498a7f62aa1ef26b54a13f81cb9c6
ajithcravi <craviajith@gmail.com>
2019-11-09 14:08:29 +0530

    adding 204 errorcodes

--- conference/server/index.js
+++ conference/server/index.js
@@ -5,7 +5,9 @@ const routes = require("./routes");
 app.use(express.static("public"));
 app.use("/", routes());
 
-// app.get('/favico', (req,res,next))
+app.get("/favico", (req, res, next) => {
+  res.sendStatus(204);
+});
 app.listen(3000, () => {
   console.log("I am an express server listening in port 3000");
 });
